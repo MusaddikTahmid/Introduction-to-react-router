83ed5128652adf480b7c45ab9afc5e0771bf6681
MusaddikTahmid <musaddiktahmid@gmail.com>
2024-03-22 21:02:07 +0600

    many hook added

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,14 @@ import Contact from "./components/Contact/Contact.jsx";
 import Users from "./components/Users/Users.jsx";
 import UserDetails from "./components/userDetails/UserDetails.jsx";
 import Posts from "./components/Posts/Posts.jsx";
+import PostDetails from "./components/PostDetails/PostDetails.jsx";
+import ErrorPage from "./components/ErrorPage/ErrorPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home></Home>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/about",
@@ -38,6 +41,12 @@ const router = createBrowserRouter([
         path: "/posts",
         loader: () => fetch(`https://jsonplaceholder.typicode.com/posts`),
         element: <Posts></Posts>,
+      },
+      {
+        path: "/post/:postId",
+        loader: ({ params }) =>
+          fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`),
+        element: <PostDetails></PostDetails>,
       },
     ],
   },
